79bcd929cfbbea75712803704c2e63a5b6d731df
Ekaterina Anplitova <anplitova@yandex.ru>
2026-03-30 13:36:53 +0300

    Initial project setup

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import vueParser from 'vue-eslint-parser';
+import typescriptParser from '@typescript-eslint/parser';
+import vuePlugin from 'eslint-plugin-vue';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/*.config.js',
+      '**/*.config.ts',
+    ],
+  },
+  {
+    files: ['**/*.{js,ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: vueParser,
+      parserOptions: {
+        parser: typescriptParser,
+        ecmaFeatures: {
+          jsx: true,
+        },
+        extraFileExtensions: ['.vue'],
+        sourceType: 'module',
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      vue: vuePlugin,
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/no-multiple-template-root': 'off',
+      'vue/no-v-html': 'warn',
+      'vue/require-default-prop': 'off',
+      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+  },
+];
